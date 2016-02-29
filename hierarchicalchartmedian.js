91ead77a88b8006d52e98b715b609f10eb6e3b7e
Claire Gerson <clairegerson@gmail.com>
2016-02-28 23:10:38 -0500

    update sort function

--- hierarchicalchartmedian.js
+++ hierarchicalchartmedian.js
@@ -37,7 +37,8 @@ var sample_data = {
 
 var margin = {top: 80, right: 100, bottom: 30, left: 300},
     width = 1000 - margin.left - margin.right,
-    height = 1000 - margin.top - margin.bottom;
+    height = 5000
+    //height = 5000 - margin.top - margin.bottom;
 
 var x = d3.scale.linear()
     .domain([0, 52]).nice()
@@ -51,8 +52,8 @@ var barHeight = 15;
 var color = d3.scale.ordinal()
     .range(["#a943b7","#ccc"]);
 
-var duration = 600,
-    delay = 25;
+var duration = 400,
+    delay = 15;
 
 var partition = d3.layout.partition()
     .value(function(d) { return d.median; })
@@ -62,27 +63,34 @@ var partition = d3.layout.partition()
 var xAxis = d3.svg.axis()
     .scale(x)
     .orient("top");
-/**
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left");
-**/
 
 var input = d3.select("#checkBoxDiv")
     .append("input")
     .attr("type","checkbox")
-    .style("position","relative");
+    //.attr("checked","false")
+    .style("position","relative")
+    .on("change", check);
 
 var inputLabel = d3.select("#checkBoxDiv")
     .append("text")
     .attr("class","label")
     .style("position","relative")
     .text("Sort Alphabetically");
+/**
+d3.select("#container")
+    .append("svg:svg")
+    .attr("width", 2000)//containerWidth)
+      .attr("height", 2000);//containerHeight);
+**/
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
+var svg_width = width + margin.left + margin.right;
+var svg_height = height + margin.top + margin.bottom;
+
+var svg = d3.select("#container").append("svg")
+    .attr("viewBox", "0,0,"+svg_width+",5000")
+    //.attr("width", svg_width)
+    //.attr("height", svg_height)
+    .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 svg.append("rect")
@@ -114,39 +122,44 @@ svg.append("text")
     .attr("text-anchor", "left")
     .attr("x", 0)
     .attr("y", 20 - margin.top)
-        .text("Click on an Industry name for more information. Click on background to return to previous view.");
+        .text("Click on an industry name to find company-level information. Click on background to return to previous view.");
+
+var rootData;
 
 var file_name = "ML_data_paid_median.json";
 
 d3.json(file_name, function(error, data) {
 
-    console.log(data);
-
     if (error) throw error;
+
+    rootData = data; // save root data object
     
     partition.nodes(data);
-    //x.domain([0, 52]).nice();
+    x.domain([0, 52]).nice();
+    
     down(data, 0);
 
-    d3.select("input").on("change", change);
-    
-    function change(){
-
-    console.log("entered change");
-
-        var sortalpha = function(a,b) {return d3.descending(b.key,a.key);};
-        var sortmedian = function(a,b) {return b.median - a.median;};
-
-    partition.sort(this.checked ? sortalpha : sortmedian);
-    //partition.sort(function(a,b) {return d3.descending(b.key,a.key);});
-    partition.nodes(data);
-    down(data, 0);
-    }
-    
 });
 
+function check(){
 
-function down(d, i) {
+    console.log(this.checked);
+
+    var sortalpha = function(a,b) {return d3.descending(b.key, a.key);};
+    var sortmedian = function(a,b) {return b.median - a.median;};
+
+    // Grab current data object
+    // This is hacky
+    data = svg.selectAll("rect")[0][0].__data__;
+
+    partition.sort(this.checked ? sortalpha : sortmedian);
+    partition.nodes(rootData); // sort all data from root
+
+    down(data, 0); // draw starting at current data object
+    
+}
+
+function down(d, i) { 
     
     if (!d.children || this.__transition__) return;
     var end = duration + d.children.length * delay;
@@ -161,8 +174,8 @@ function down(d, i) {
     var exitLabel = svg.selectAll(".industryType")
         .attr("class", "exit");
 
-    //Mean line
-    var exitMeanLine = svg.selectAll(".meanLine")
+    //Median line
+    var exitMedianLine = svg.selectAll(".medianLine")
         .attr("class", "exit");
 
   // Entering nodes immediately obscure the clicked-on bar, so hide it. 
@@ -172,8 +185,9 @@ function down(d, i) {
   // Enter the new bars for the clicked-on data.
   // Per above, entering bars are immediately visible.
     var enter = bar(d)
+    //.attr("transform", function(d, i) { return "translate(0," + barHeight * i * 1.2 + ")"; })
       .attr("transform", stack(i))
-      .style("opacity", 1);
+        .style("opacity", 1);
 
   // Have the text fade-in, even though the bars are visible.
   // Color the bars as parents; they will fade to children if appropriate.
@@ -187,12 +201,10 @@ function down(d, i) {
     .attr("text-anchor", "left")
     .attr("x", 0)
     .attr("y", 50 - margin.top)
-        .text(d.key.toString());
+        .text(d.key.toString().toUpperCase());
 
   // Update the x-scale domain.
   //x.domain([0, d3.max(d.children, function(d) { return d.median; })]).nice();
-
-    //y.domain([height, d3.max(d.children, function(d) { return d.median; })]);
     
     // Update the x-axis.
     // why do I need to update if not changing domain ???
@@ -203,7 +215,9 @@ function down(d, i) {
   // Transition entering bars to their new position.
   var enterTransition = enter.transition()
       .duration(duration)
-      .delay(function(d, i) { return i * delay; })
+      //.duration(0)
+    .delay(function(d, i) { return i * delay; })
+    //.delay(function(d, i) { return i; })
       .attr("transform", function(d, i) { return "translate(0," + barHeight * i * 1.2 + ")"; });
 
   // Transition entering text.
@@ -220,10 +234,10 @@ function down(d, i) {
       .duration(duration)
       .style("opacity", 1e-6)
       .remove();
+    
+    exitLabel.transition().duration(0).remove();
 
-    exitLabel.transition().duration(1).remove();
-
-    exitMeanLine.transition().duration(1).remove();
+    exitMedianLine.transition().duration(1).remove();
 
     // Transition exiting bars to the new x-scale.
   exitTransition.selectAll("rect")
@@ -231,11 +245,14 @@ function down(d, i) {
     
   // Rebind the current node to the background.
   svg.select(".background")
-      .datum(d)
+        .datum(d)
     .transition()
-      .duration(end);
+        .duration(end);
 
-  d.index = i;
+    d.index = i;
+
+    // Median line
+    drawMedian(d);
 }
 
 function up(d) {
@@ -252,8 +269,8 @@ function up(d) {
     var exitLabel = svg.selectAll(".industryType")
         .attr("class", "exit");
 
-    //Mean line
-    var exitMeanLine = svg.selectAll(".meanLine")
+    //Median line
+    var exitMedianLine = svg.selectAll(".medianLine")
         .attr("class", "exit");
 
     // Enter the new bars for the clicked-on data's parent.
@@ -265,9 +282,9 @@ function up(d) {
   // Color the bars as appropriate. NO
   // Exiting nodes will obscure the parent bar, so hide it.
     enter.select("rect")
-        .style("fill", function(d) { return color(!!d.children); })
-        .filter(function(p) { return p === d; })
-    .style("fill-opacity", 1e-6);
+        .style("fill", function(d) { return color(!!d.children); });
+        //.filter(function(p) { return p === d; })
+    //.style("fill-opacity", 1e-6);
 
     svg.select("g")
     .append("text")
@@ -275,15 +292,12 @@ function up(d) {
     .attr("text-anchor", "left")
     .attr("x", 0)
     .attr("y", 50 - margin.top)
-        .text(d.parent.key.toString());
+        .text(d.parent.key.toString().toUpperCase());
 
     
   // Update the x-scale domain.
     //x.domain([0, d3.max(d.parent.children, function(d) { return d.value/d.count; })]).nice();
 
-        //y.domain([height, d3.max(d.parent.children, function(d) { return d.median; })]);
-
-    
     // Update the x-axis.
   svg.selectAll(".x.axis").transition()
      .duration(duration)
@@ -304,10 +318,12 @@ function up(d) {
   // Transition exiting bars to the parent's position.
     var exitTransition = exit.selectAll("g")
         .transition()
-      .duration(duration)
-      .delay(function(d, i) { return i * delay; })
-        .attr("transform", stack(d.index));
-       
+    .duration(duration)
+    //.duration(0)
+    .delay(function(d, i) { return i * delay; })
+    //.delay(function(d, i) { return 0; })
+        //.attr("transform", stack(d.index));
+        .attr("transform",function(d, i) { return "translate(0," + barHeight * d.index * 1.2 + ")"; });
 
   // Transition exiting text to fade out.
   exitTransition.select("text")
@@ -320,19 +336,24 @@ function up(d) {
       .style("fill", color(true));
     
   // Remove exiting nodes when the last child has finished transitioning.
-  exit.transition()
-      .duration(end)
+    exit.transition()
+    .duration(end)
+    //.duration(duration)
         .remove();
 
-    exitLabel.transition().duration(1).remove();
+    exitLabel.transition().duration(0).remove();
 
-    exitMeanLine.transition().duration(1).remove();
+    exitMedianLine.transition().duration(1).remove();
     
   // Rebind the current parent to the background.
   svg.select(".background")
-      .datum(d.parent)
-    .transition()
-      .duration(end);
+        .datum(d.parent)
+        .transition()
+       // .duration(duration);
+        .duration(end);
+
+    // Median line
+    drawMedian(d.parent);
 }
 
 // Creates a set of bars for the given data node, at the specified index.
@@ -359,8 +380,6 @@ function bar(d) {
         .attr("width", function(d) { return x(d.median); })
         .attr("height", barHeight);
 
-    console.log("bar function");
-
   return bar;
 }
 
@@ -371,8 +390,30 @@ function stack(i) {
   return function(d) {
       var tx = "translate(" + x0 + "," + barHeight * i * 1.2 + ")";
       x0 += x(d.median);
+      //x0 = x(0)
       console.log("stack");
     return tx;
   };
 
+}
+
+//Draws a vertical line at average weeks of leave within industry
+function drawMedian(d){
+
+    var m = d3.median(d.children, function(d) { return d.median; });
+    var l = d.children.length;
+
+    console.log("median", m);
+
+    var median =
+        svg.select("g")
+        .append("line")
+        .attr("class", "medianLine")
+        .style("stroke", "black")
+        .attr("x1", x(m))
+        .attr("y1", 0)
+        .attr("x2", x(m))
+        .attr("y2", l*barHeight*1.2);
+
+    return median;
 }
